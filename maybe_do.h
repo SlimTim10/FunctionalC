6d8c5731260315315df78a88659a1e517c97d618
SlimTim10 <slimtim10@gmail.com>
2018-03-04 16:17:39 -0500

    Fix formatting

--- maybe_do.h
+++ maybe_do.h
@@ -12,32 +12,32 @@
 #define maybe_do9(x, f, ...) ( maybe_do8(bind(f, x), __VA_ARGS__) )
 #define maybe_do10(x, f, ...) ( maybe_do9(bind(f, x), __VA_ARGS__) )
 
-#define GET_MACRO(_1,							\
-				  _2,							\
-				  _3,							\
-				  _4,							\
-				  _5,							\
-				  _6,							\
-				  _7,							\
-				  _8,							\
-				  _9,							\
-				  _10,							\
-				  _11,							\
-				  NAME,							\
-				  ...) NAME
+#define GET_MACRO(_1,\
+	_2,\
+	_3,\
+	_4,\
+	_5,\
+	_6,\
+	_7,\
+	_8,\
+	_9,\
+	_10,\
+	_11,\
+	NAME,\
+	...) NAME
 
-#define maybe_do(...)													\
-	GET_MACRO(__VA_ARGS__,												\
-			  maybe_do10,												\
-			  maybe_do9,												\
-			  maybe_do8,												\
-			  maybe_do7,												\
-			  maybe_do6,												\
-			  maybe_do5,												\
-			  maybe_do4,												\
-			  maybe_do3,												\
-			  maybe_do2,												\
-			  maybe_do1)												\
+#define maybe_do(...)\
+	GET_MACRO(__VA_ARGS__,\
+		maybe_do10,\
+		maybe_do9,\
+		maybe_do8,\
+		maybe_do7,\
+		maybe_do6,\
+		maybe_do5,\
+		maybe_do4,\
+		maybe_do3,\
+		maybe_do2,\
+		maybe_do1)\
 	(__VA_ARGS__)
 
 #define mstart ( mreturn(NULL) )
